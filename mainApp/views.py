8831287a3825446539cc0d356056dbdba3a8e0de
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-22 00:14:48 -0600

    added insight list page

--- mainApp/views.py
+++ mainApp/views.py
@@ -14,10 +14,12 @@ def indexPage(request):
     {
         "all_service":services,
         "5_service":services[0:5],
+        "all_researchInfor3":models.ResearchInsightInfo.objects.all()[0:3],
         "all_researchInfor":models.ResearchInsightInfo.objects.all()
 
     }
     )
+
 
 
 
@@ -38,7 +40,8 @@ def team(request):
 def insightList(request):
     return render(request,'insight-list.html',
     {
-       "all_service" :models.ServiceModel.objects.all()
+       "all_service" :models.ServiceModel.objects.all(),
+        "all_researchInfor":models.ResearchInsightInfo.objects.all()
     })
 
 
